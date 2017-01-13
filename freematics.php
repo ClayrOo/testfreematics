ca4c78dcddb0ac8f3398c90145ca21e57c0a94fb
ClayrOo <cbassil@deloitte.com>
2017-01-13 14:06:56 +0100

    Create freematics.php

---
+++ freematics.php
@@ -0,0 +1,27 @@
+<?php 
+  $server_key = "TEST_SERVER_KEY"; // replace it with your server key and keep it secret
+  $server_url = "http://hub.freematics.com/".$server_key."/".$_GET["api"]."?";
+  
+  if (!$_GET["api"]) {
+    echo "No API specified";
+    exit;
+  }
+
+  // pass through all request arguments
+  foreach ($_GET as $key => $value) { 
+    if ($key !== "api") {
+      $server_url .= $key."=".$value."&";
+    }
+  } 
+  
+  // invoke API
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $server_url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $result = curl_exec($ch);
+  curl_close($ch);
+
+  // forward response
+  header('Content-Type: application/json');
+  echo $result;
+?>
